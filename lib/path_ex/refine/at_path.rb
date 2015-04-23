ed9614b5a0dd54e4aeb745d63c6e5f48872bb63b
Brasten Sager <brasten@brasten.me>
2015-04-23 09:30:27 -0700

    Added #set_at method

--- lib/path_ex/refine/at_path.rb
+++ lib/path_ex/refine/at_path.rb
@@ -2,6 +2,16 @@ module PathEx::Refine
   module AtPath
 
     refine Hash do
+      def set_at(key, value)
+        key = PathEx.key(key)
+
+        if key.has_tail?
+          (self[key.head.to_s] ||= {}).set_at(key.tail, value)
+        else
+          self[key.head.to_s] = value
+        end
+      end
+
       def at(*keys)
         key = PathEx.key(keys.join('.'))
         return self if key.empty?
@@ -16,12 +26,28 @@ module PathEx::Refine
     end
 
     refine NilClass do
+      def set_at(key, value)
+        nil
+      end
+
       def at(*keys)
         nil
       end
     end
 
     refine Object do
+      def set_at(key, value)
+        key = PathEx.key(key)
+
+        if key.has_tail?
+          # If the method doesn't exist, this will explode. OK for now.
+          self.__send__(key.head.to_sym).set_at(key.tail, value)
+        else
+          self.__send__(:"#{key.head.to_s}=", value)
+        end
+
+      end
+
       def at(*keys)
         key = PathEx.key(keys.sum)
         return self if key.empty?
